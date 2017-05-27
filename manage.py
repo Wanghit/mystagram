adb079ca52d389787d61d31aa55e0e734fb0238c
wanghuan <1844804494@qq.com>
2017-05-27 21:40:29 +0800

    web for image share

--- manage.py
+++ manage.py
@@ -18,11 +18,16 @@ def init_database():
         db.session.add(User('User'+str(i+1),'a'+str(i)))
         for j in range(0,3):
             db.session.add(Image(get_image_url(),i+1))
-            for k in range(0,3):
-                db.session.add(Comment('This is '+str(k)+'comment',1+3*i+j,i+1))
+
+    for k in range(1,301):
+        for h in range(0,3):
+            db.session.add(Comment('This is '+str(h)+'comment',k,random.randint(1,100)))
 
     db.session.commit()
-
+@manager.command
+def pp():
+    for i in range(0,3):
+        print i
 @manager.command
 def sp():
     #print 1, User.query.all()
@@ -34,8 +39,27 @@ def sp():
     #print User.query.filter(and_(User.id >88, User.id < 99)).first_or_404()
     #分页
     #print User.query.order_by(User.id.desc()).paginate(page=1,per_page=10).items
-    user=User.query.get(1)
-    print user.images
+    #user=User.query.get(1)
+    #print user.images
+    image=Image.query.get(1)
+    print image.user
+
+@manager.command
+def up():
+    for i in range(50,100,2):
+        user=User.query.get(i)
+        user.username='[New2]'+user.username
+    #db.session.commit()
+    for i in range(11,20):
+         User.query.filter_by(id=i).update({'username':'[New3]'+User.query.get(i).username})
+    db.session.commit()
+@manager.command
+def dele():
+    for i in range(50,100,2):
+        comment=Comment.query.get(i)
+        db.session.delete(comment)
+    db.session.commit()
+
 if __name__=='__main__':
     manager.run()
 #
